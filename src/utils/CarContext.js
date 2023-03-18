6d46e4de83ed266453f8bfaa9d664982d54c2013
Leo Wong <leoelicos@gmail.com>
2023-03-19 04:00:13 +1100

    Provider replaces useContext.Provider

--- src/utils/CarContext.js
+++ src/utils/CarContext.js
@@ -1,32 +1,15 @@
-import React, { useContext } from 'react';
+import React from 'react'
+import { Provider } from 'react-redux'
 
-// Create our car context using React.CreateContext()
-export const CarContext = React.createContext();
+// Import Redux store from the file we created in /utils
+import store from './store'
 
-// Create a custom hook that allows easy access to our CarContext values
-export const useCar = () => useContext(CarContext);
-
-const randomNum = () => Math.floor(Math.random() * 20000);
-
-// Creating our car provider. Accepts an argument of `props`
 export default function CarProvider(props) {
-  const cars = [
-    {
-      id: randomNum(),
-      make: 'Honda',
-      model: 'Civic',
-      year: '2008',
-      isRunning: false,
-    },
-    {
-      id: randomNum(),
-      make: 'Tesla',
-      model: 'Y',
-      year: '2021',
-      isRunning: false,
-    },
-  ];
-
-  // The provider component will wrap all other components inside of it that need access to our global state
-  return <CarContext.Provider value={{ cars }} {...props} />;
+  // Return the provider component provided by react-redux
+  return (
+    <Provider
+      store={store}
+      {...props}
+    />
+  )
 }
