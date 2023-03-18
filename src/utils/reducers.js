8e8adae98d583448f24fa5f3539412a21baccc65
Leo Wong <leoelicos@gmail.com>
2023-03-19 03:48:34 +1100

    Install dependencies. Add public folder

--- src/utils/reducers.js
+++ src/utils/reducers.js
@@ -1,42 +1,42 @@
-import { ADD_CAR, START_CAR, STOP_CAR } from '../utils/actions';
+import { ADD_CAR, START_CAR, STOP_CAR } from '../utils/actions.js'
 
 export default function reducer(state, action) {
   switch (action.type) {
     case ADD_CAR: {
-      const newCarId = state.cars[state.cars.length - 1].id + 1;
-      const newCar = { ...action.payload, id: newCarId };
+      const newCarId = state.cars[state.cars.length - 1].id + 1
+      const newCar = { ...action.payload, id: newCarId }
 
       return {
         ...state,
-        cars: [...state.cars, newCar],
-      };
+        cars: [...state.cars, newCar]
+      }
     }
     case START_CAR: {
-      const carIndex = state.cars.findIndex((car) => car.id === action.payload);
-      const updatedCar = { ...state.cars[carIndex], isRunning: true };
+      const carIndex = state.cars.findIndex((car) => car.id === action.payload)
+      const updatedCar = { ...state.cars[carIndex], isRunning: true }
 
-      const carsCopy = [...state.cars];
-      carsCopy[carIndex] = updatedCar;
+      const carsCopy = [...state.cars]
+      carsCopy[carIndex] = updatedCar
 
       return {
         ...state,
-        cars: carsCopy,
-      };
+        cars: carsCopy
+      }
     }
     case STOP_CAR: {
-      const carIndex = state.cars.findIndex((car) => car.id === action.payload);
-      const updatedCar = { ...state.cars[carIndex], isRunning: false };
+      const carIndex = state.cars.findIndex((car) => car.id === action.payload)
+      const updatedCar = { ...state.cars[carIndex], isRunning: false }
 
-      const carsCopy = [...state.cars];
-      carsCopy[carIndex] = updatedCar;
+      const carsCopy = [...state.cars]
+      carsCopy[carIndex] = updatedCar
 
       return {
         ...state,
-        cars: carsCopy,
-      };
+        cars: carsCopy
+      }
     }
     default: {
-      return state;
+      return state
     }
   }
 }
