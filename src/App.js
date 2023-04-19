8520347c8b11273c0b678745c3d35acfb1d193f1
Leo Wong <leoelicos@gmail.com>
2023-04-19 22:25:10 +1000

    Import CSS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import CarComponent from './components/CarComponent.js'
-// Importing our theme provider which will make our global state available to child components
 import CarProvider from './utils/CarContext.js'
-
+import './app.css'
 export default function App() {
   useEffect(() => {
     document.title = 'Vroom'
